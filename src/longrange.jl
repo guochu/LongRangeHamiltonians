98aa31ebb00d7ce0bd7f42d2887c679605863d78
guo chu <guochu604b@gmail.com>
2024-01-14 21:46:37 +0800

    gc changes

--- src/longrange.jl
+++ src/longrange.jl
@@ -3,4 +3,4 @@ abstract type AbstractLongRangeTerm end
 DMRG.space_l(x::AbstractLongRangeTerm) = isa(x.a, MPOTensor) ? space_l(x.a) : oneunit(spacetype(x))
 DMRG.space_r(x::AbstractLongRangeTerm) = isa(x.b, MPOTensor) ? space_r(x.b) : oneunit(spacetype(x))'
 TK.spacetype(x::AbstractLongRangeTerm) = spacetype(typeof(x))
-DMRG.coeff(x::AbstractLongRangeTerm) = x.coeff
+coeff(x::AbstractLongRangeTerm) = x.coeff
